b6e2e333657bf16d2c1ffed965680ba60749fdaf
YouwenShui <androidforwork@163.com>
2018-06-18 18:48:55 +0800

    init...

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    if (a > 10)
+    {
+        printf("a大于10");
+    }
+    else
+    {
+        printf("a小于10");
+    }
+    printf("Hello world!");
+}
